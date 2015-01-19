dedc631351a9138ce60a8849058b78b00e3e9664
Kamil Mówiński <kamil@mowinski.com.pl>
2015-01-19 13:44:21 +0100

    Tracker got a param with joint

--- scripts/tracker.py
+++ scripts/tracker.py
@@ -5,40 +5,36 @@ import math
 from std_msgs.msg import Header
 from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import Quaternion
-from my_kinnect.msg import Head
+from my_kinnect.msg import SkeletonCoords
 
-class HeadTracker():
+class Tracker():
 
     def __init__(self):
-        rospy.init_node('head_kinect', anonymous=True)
+        rospy.init_node('tracker_mykinect', anonymous=True)
 	self.myFrame = 'kinect_link'
-	self.pub_trans = rospy.Publisher(self.myFrame, Head, queue_size=10)
+	self.pub_trans = rospy.Publisher(self.myFrame, SkeletonCoords, queue_size=10)
         rate = rospy.get_param('~rate', 3)
+
         r = rospy.Rate(rate)
-        self.target_joint = rospy.get_param('~target_joint', '/head')
-        # ramka ktorej transformacji poszykujemy to /head + _nrUzytkownika, tu head_1
-	self.target_joint += '_1'
+        self.target_joint = "{0}_1".format(rospy.get_param('~target_joint', '/head'))
 
-	# Zainicjalizuj TransformListener
         self.tf = tf.TransformListener()
-        rospy.loginfo("Rozpoczynam sledzenia uzytkownika za 5s...")
-
-        # Zaczekaj 5s aby bufor TransformListenera sie zapelnil
+        rospy.loginfo("Start tracking for 5s...")
         rospy.sleep(5.0)
-        rospy.loginfo("Sledzenie rozpoczete!")
+        rospy.loginfo("Tracking started!")
 	
         while not rospy.is_shutdown():
 	    trans, rot = self.tf.lookupTransform('/openni_depth_frame', self.target_joint, rospy.Duration())
             vec = Vector3(*trans)
             rt = Quaternion(*rot)
             h = Header(stamp=rospy.Time.now())
-            head = Head(trans=vec, rot=rt, header=h)
+            head = SkeletonCoords(trans=vec, rot=rt, header=h)
             self.pub_trans.publish(head)
             r.sleep()
 
 if __name__ == '__main__':
     try:
-        HeadTracker()
+        Tracker()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
